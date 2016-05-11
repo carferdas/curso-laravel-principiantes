2a816fdf1d47f095a30658ef570b7b927706ac5d
Carlos Fernandes <informanimo@gmail.com>
2016-05-11 00:12:12 -0430

    Agregado campo avatar a la migracion de users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ class CreateUsersTable extends Migration
             $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('avatar');
             $table->enum('type', ['admin', 'member'])->default('member');
             $table->rememberToken();
             $table->timestamps();
