b00a2595e542074e13ca93c2bd476f55906b0254
Carlos Fernandes <informanimo@gmail.com>
2016-05-11 00:18:10 -0430

    Agregado campo avatar al modelo User

--- app/User.php
+++ app/User.php
@@ -8,7 +8,7 @@ class User extends Authenticatable
 {
     
     protected $fillable = [
-        'first_name', 'last_name', 'email', 'password', 'type'
+        'first_name', 'last_name', 'email', 'password', 'avatar','type'
     ];
 
     
